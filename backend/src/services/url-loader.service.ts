ad24f2e944d573c9a83cf7f7c5d4616a373d45e0
hamdidemir <128630855+hamdidemir@users.noreply.github.com>
2023-07-10 23:53:49 +0300

    Update url-loader.service.ts

--- backend/src/services/url-loader.service.ts
+++ backend/src/services/url-loader.service.ts
@@ -1,32 +1,49 @@
-import puppeteer, { Browser } from 'puppeteer'
+import puppeteer, { Browser } from 'puppeteer';
 
-import { domExtractHyperlinks, domExtractText, pageEval } from './page-eval.service.js'
-
-export interface TextAndLinks{
-  text: string
-  links: string[]
+export interface TextAndLinks {
+  text: string;
+  links: string[];
 }
 
 export class UrlLoaderService {
-  private static instance: UrlLoaderService
+  private static instance: UrlLoaderService;
 
-  static async getInstance (): Promise<UrlLoaderService> {
+  static async getInstance(): Promise<UrlLoaderService> {
     if (UrlLoaderService.instance === undefined) {
-      const browser = await puppeteer.launch()
-      UrlLoaderService.instance = new UrlLoaderService(browser)
+      const browser = await puppeteer.launch();
+      UrlLoaderService.instance = new UrlLoaderService(browser);
     }
-    return UrlLoaderService.instance
+    return UrlLoaderService.instance;
   }
 
-  private constructor (private readonly browser: Browser) {
-  }
+  private constructor(private readonly browser: Browser) {}
 
-  async loadUrlTextAndLinks (url: string): Promise<TextAndLinks> {
-    const page = await this.browser.newPage()
-    await page.goto(url)
-    await page.waitForSelector('body')
-    const [text, links] = await Promise.all([await pageEval(page, domExtractText), await pageEval(page, domExtractHyperlinks)])
-
-    return { text, links }
+  async loadUrlTextAndLinks(url: string, level: number): Promise<TextAndLinks> {
+    const page = await this.browser.newPage();
+  
+    try {
+      if (!url.endsWith('.pdf')) {
+        await page.goto(url);
+        await page.waitForSelector('body');
+  
+        const [text, links] = await Promise.all([
+          page.evaluate(() => document.body.innerText),
+          page.evaluate(() => Array.from(document.getElementsByTagName('a'), (a) => a.href)),
+        ]);
+  
+        return { text, links };
+      } else {
+        return { text: '', links: [] };
+      }
+    } catch (error) {
+      console.error(`Error loading URL: ${url}`, error);
+      return { text: '', links: [] };
+    } finally {
+      await page.close();
+    }
   }
+  
+  
 }
+
+export default UrlLoaderService;
